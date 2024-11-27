bcb53af09e7b569e068a39aa0edc23fcb7fdd0e0
Giovanni <giojar06@gmail.com>
2024-11-27 11:02:56 -0600

    Create calculadora-Suma.cpp
    Correciones en la suma

---
+++ calculadora-Suma.cpp
@@ -0,0 +1,9 @@
+switch (opcion) {
+            case 1: // Suma
+                cout << "Ingrese un numero: ";
+                cin >> num1;
+                cout << "Ingrese otro numero: ";
+                cin >> num2;
+                resultado = num1 + num2;
+                cout << "El resultado es: " << resultado << endl;
+                break;
